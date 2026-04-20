4b5566ae7ddac661242bf70b2711b0e1435e1bab
Mohamed198080 <mohamed.a.sabry.80@gmail.com>
2026-04-20 20:25:14 +0300

    Update API_BASE URL in config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_BASE = "https://script.google.com/macros/s/AKfycbxP2NuM1GZVhsrtdDMW8W-Zi9u31_rr4T91TG4BK4rb-6VRyAAE7V5ij8TGl7TzlAxclg/exec"; // استبدل
+const API_BASE = "https://script.google.com/macros/s/AKfycbyMAiYK9oVmpsczGIl8ksoJL6FRHEX5hh6_l1cmqRhkZI5vQbFwYakYbpkf_P5-y8ZC8Q/exec"; // استبدل
