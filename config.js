ac978e015ec6fe029b734bd8a18f58156a6fba3a
Mohamed198080 <mohamed.a.sabry.80@gmail.com>
2026-04-20 20:34:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_BASE = "https://script.google.com/macros/s/AKfycbyMAiYK9oVmpsczGIl8ksoJL6FRHEX5hh6_l1cmqRhkZI5vQbFwYakYbpkf_P5-y8ZC8Q/exec"; // استبدل
+const API_BASE = "https://script.google.com/macros/s/AKfycbxxBSZT16O-ksIzz9w0jn9rSxylQOTW3GIa9i6XfO5b96ABfuamcW1w60-jnxp8kXvHLA/exec"; // استبدل
